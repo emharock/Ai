8081371e97c375d7336f54046acd6c16cd72e35c
emharock <marrionemarcuzsean@gmail.com>
2023-10-01 21:40:50 +0800

    first commit

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.post('/', async (req, res) => {
         });
 
         res.status(200).send({
-            bot: response.data.choices[0].text
+            bot: response.choices[0].text
         })
     } catch (error) {
         console.log(error);
